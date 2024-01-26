581e210db69ac0d392ba01c4944c49f111178c3e
seedevk8s <seedevk8s@gmail.com>
2024-01-26 18:34:50 +0900

    7.5.3 까지. 블로그 글 수정기능 추가 아직 수정 내용 반영여부는 확인해야함

--- src/main/java/com/generator/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/com/generator/springbootdeveloper/controller/BlogViewController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -34,6 +35,18 @@ public class BlogViewController {
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    // id 키를 가진 쿼리 파라미터의 값을 id 변수에 매핑 (id는 없을 수도 있음)
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) {           // id가 없으면 생성
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {                    // id가 있으면 수정
+            Article2 article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+        return "newArticle";
     }
 
 }
--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -14,6 +14,64 @@ if (deleteButton) {
     })
 }
 
+// 수정 기능
+// id가 modify-btn인 엘리먼트 조회
+const modifyButton = document.getElementById('modify-btn');
+
+if (modifyButton) {
+    // 클릭 이벤트가 감지되면 수정 API 요청
+    modifyButton.addEventListener('click', event => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id')
+
+        fetch(`/api/articles/${id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value
+            })
+        })
+            .then(() => {
+                alert("수정이 완료되었습니다.")
+                location.replace(`/articles/${id}`);
+            })
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
