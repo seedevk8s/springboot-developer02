0c9bf6d0d7d3178f5478999aa4104a6b88dba691
seedevk8s <seedevk8s@gmail.com>
2024-02-02 11:46:56 +0900

    8장 스프링 시큐리티로 로그인/로그아웃, 회원가입 구현 확인 완료 지금까지 내용다 확인 완료

---
+++ src/main/java/com/generator/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,51 @@
+package com.generator.springbootdeveloper.service;
+
+import com.generator.springbootdeveloper.domain.User;
+import com.generator.springbootdeveloper.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+// 스프링 시큐리티에서 사용자 정보를 가져오는 인터페이스
+public class UserDetailService implements UserDetailsService {
+
+    private  final UserRepository userRepository;
+
+    // 사용자 이름(email)으로 사용자의 정보를 가져오는 메서드
+    @Override
+    public User loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/generator/springbootdeveloper/service/UserService.java
@@ -0,0 +1,53 @@
+package com.generator.springbootdeveloper.service;
+
+import com.generator.springbootdeveloper.domain.User;
+import com.generator.springbootdeveloper.dto.AddUserRequest;
+import com.generator.springbootdeveloper.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                // 1) 패스워드 암호화
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
